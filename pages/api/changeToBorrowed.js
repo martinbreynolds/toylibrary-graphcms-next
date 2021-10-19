771c8808f46db31a4f2084b06eab3a5bc895b87c
Martin Reynolds <62625037+martinbreynolds@users.noreply.github.com>
2021-10-19 13:46:08 +0100

    Create changeToBorrowed.js

---
+++ pages/api/changeToBorrowed.js
@@ -0,0 +1,35 @@
+import { GraphQLClient } from 'graphql-request'
+
+export default async ({ body}, res) => {
+    const url = process.env.ENDPOINT
+    const graphcms = new GraphQLClient(url, {
+        headers: { "Authorization" : process.env.GRAPH_CMS_TOKEN}
+    })
+
+    await graphcms.request(
+        `
+        mutation($slug: String!) {
+          updateToy(where: 
+            { slug: $slug}, 
+            data: { seen: true}
+          ) {
+            id,
+            name,
+            borrowed
+          }
+        }
+        `,
+        { slug: body.slug }
+    )
+
+    await graphcms.request(
+        `mutation publishToy($slug: String) {
+        publishToy(where: { slug: $slug}, to: PUBLISHED) {
+            slug
+            }
+        }`,
+        { slug: body.slug }
+    )
+
+    res.status(201).json({ slug: body.slug })
+}
