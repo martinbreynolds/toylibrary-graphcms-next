88e687fa0f01096737190f17880fadbbdbbe6fb0
Martin Reynolds <martinandtoni@btinternet.com>
2021-10-28 00:15:14 +0100

    add return statement

--- components/toys/addToy.js
+++ components/toys/addToy.js
@@ -28,60 +28,60 @@ export default function AddToy() {
   //   router.reload(window.location.pathname);
   // };
 
-  // return (
-  //   <div className="mb-3 bg-teal p-3 rounded-lg">
-  //     <p className="text-white text-md font-bold mb-3">Create a New Member</p>
-  //     <form onSubmit={newMember}>
-  //       <div className="grid grid-cols-4">
-  //         <label className="text-white" htmlFor="fName">
-  //           First Name
-  //         </label>
-  //         <label className="text-white" htmlFor="lName">
-  //           Last Name
-  //         </label>
-  //         <label className="text-white" htmlFor="email">
-  //           Email Address
-  //         </label>
-  //       </div>
-  //       <div className="grid grid-cols-4 ">
-  //         <input
-  //           className="text-plum font-bold rounded-lg mr-3"
-  //           name="fName"
-  //           autoComplete="First Name"
-  //           type="text"
-  //           required
-  //           onChange={(event) => setFName(event.target.value)}
-  //           value={fName}
-  //         />
+  return (
+    //   <div className="mb-3 bg-teal p-3 rounded-lg">
+    //     <p className="text-white text-md font-bold mb-3">Create a New Member</p>
+    //     <form onSubmit={newMember}>
+    //       <div className="grid grid-cols-4">
+    //         <label className="text-white" htmlFor="fName">
+    //           First Name
+    //         </label>
+    //         <label className="text-white" htmlFor="lName">
+    //           Last Name
+    //         </label>
+    //         <label className="text-white" htmlFor="email">
+    //           Email Address
+    //         </label>
+    //       </div>
+    //       <div className="grid grid-cols-4 ">
+    //         <input
+    //           className="text-plum font-bold rounded-lg mr-3"
+    //           name="fName"
+    //           autoComplete="First Name"
+    //           type="text"
+    //           required
+    //           onChange={(event) => setFName(event.target.value)}
+    //           value={fName}
+    //         />
 
-  //         <input
-  //           className="text-plum font-bold mr-3 rounded-lg"
-  //           name="lName"
-  //           autoComplete="Last Name"
-  //           type="text"
-  //           required
-  //           onChange={(event) => setLName(event.target.value)}
-  //           value={lName}
-  //         />
+    //         <input
+    //           className="text-plum font-bold mr-3 rounded-lg"
+    //           name="lName"
+    //           autoComplete="Last Name"
+    //           type="text"
+    //           required
+    //           onChange={(event) => setLName(event.target.value)}
+    //           value={lName}
+    //         />
 
-  //         <input
-  //           className="text-plum font-bold mr-3 rounded-lg max-w-md"
-  //           name="email"
-  //           autoComplete="Email Address"
-  //           type="email"
-  //           required
-  //           onChange={(event) => setEmail(event.target.value)}
-  //           value={email}
-  //         />
+    //         <input
+    //           className="text-plum font-bold mr-3 rounded-lg max-w-md"
+    //           name="email"
+    //           autoComplete="Email Address"
+    //           type="email"
+    //           required
+    //           onChange={(event) => setEmail(event.target.value)}
+    //           value={email}
+    //         />
 
-  <button
-    type="submit"
-    className="text-white bg-plum font-bold rounded-lg py-2 px-3"
-  >
-    Create New Member
-  </button>;
-  //     </div>
-  //   </form>
-  // </div>
-  // );
+    <button
+      type="submit"
+      className="text-white bg-plum font-bold rounded-lg py-2 px-3"
+    >
+      Create New Member
+    </button>
+    //     </div>
+    //   </form>
+    // </div>
+  );
 }
