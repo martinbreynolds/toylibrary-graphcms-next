e041a88ca52d2e1c638f3b0928cf25fa1134c254
Martin Reynolds <martinandtoni@btinternet.com>
2021-11-21 20:06:42 +0000

    styling

--- components/layouts/header.js
+++ components/layouts/header.js
@@ -5,17 +5,20 @@ import navButtons from "../../config/nav";
 
 const Header = ({ session, status }) => {
   return (
-    <div className="mx-10 my-2 flex flex-row justify-between">
-      <p className="text-plum font-extrabold align-middle text-5xl">
-        Sensory Toy Library
-      </p>
-      <NavBar navButtons={navButtons} session={session} status={status} />
-      <img
-        alt="Unique Ways Logo"
-        className=" h-12"
-        src="https://www.uniqueways.org.uk/wp-content/themes/unique_ways/images/logo_footer.png"
-      />
-    </div>
+    <>
+      <div className="mx-10 my-2 flex flex-row justify-between bg-white">
+        <p className="text-plum font-extrabold align-middle text-5xl">
+          Sensory Toy Library
+        </p>
+        <NavBar navButtons={navButtons} session={session} status={status} />
+        <img
+          alt="Unique Ways Logo"
+          className=" h-12"
+          src="https://www.uniqueways.org.uk/wp-content/themes/unique_ways/images/logo_footer.png"
+        />
+      </div>
+      <div className="h-6 bg-plum "></div>
+    </>
   );
 };
 
